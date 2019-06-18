56f7628500572e57ac9bf1dc6799670e4b455a74
Anna Zubova <anna@leolog.com>
2019-06-17 17:31:06 -0700

    Update scraping functions

---
+++ insta_scrape.py
@@ -0,0 +1,102 @@
+# code taken from jnawjux with permission from owner:
+# https://github.com/jnawjux/web_scraping_corgis/blob/master/insta_scrape.py
+
+import time
+import re
+from selenium.webdriver import Chrome
+
+def recent_25_posts(username):
+    """With the input of an account page, scrape the 25 most recent posts urls"""
+    url = "https://www.instagram.com/" + username + "/"
+    browser = Chrome()
+    browser.get(url)
+    post = 'https://www.instagram.com/p/'
+    post_links = []
+    while len(post_links) < 25:
+        links = [a.get_attribute('href') for a in browser.find_elements_by_tag_name('a')]
+        for link in links:
+            if post in link and link not in post_links:
+                post_links.append(link)
+        scroll_down = "window.scrollTo(0, document.body.scrollHeight);"
+        browser.execute_script(scroll_down)
+        time.sleep(10)
+    else:
+        return post_links[:25]
+
+def insta_details(urls):
+    """Take a post url and return post details"""
+    browser = Chrome()
+    post_details = []
+    for link in urls:
+        browser.get(link)
+        try:
+        # This captures the standard like count. 
+            likes = browser.find_element_by_partial_link_text(' likes').text
+        except:
+        # This captures the like count for videos which is stored
+            view_id = '//*[@id="react-root"]/section/main/div/div/article/div[2]/section[2]/div/span'
+            print(view_id)
+            likes = browser.find_element_by_xpath(view_id).text
+        age = browser.find_element_by_css_selector('a time').text
+        xpath_c = '//*[@id="react-root"]/section/main/div/div/article/div[2]/div[1]/ul/li[1]/div/div/div'
+        print(xpath_c)
+        comment = browser.find_element_by_xpath(xpath_c).text
+        post_details.append({'link': link,'likes/views': likes,'age': age, 'comment': comment})
+        time.sleep(10)
+    return post_details
+
+def find_hashtags(comment):
+    """Find hastags used in comment and return them"""
+    hashtags = re.findall('#[A-Za-z]+', comment)
+    return hashtags
+
+############################################################
+# customize recent_25_posts function
+
+def get_n_posts(hashtag, n):
+    """With the input of a hashtag name, scrape the n most recent posts urls"""
+    url = "https://www.instagram.com/explore/tags/" + hashtag + "/"
+    browser = Chrome()
+    browser.get(url)
+    post = 'https://www.instagram.com/p/'
+    post_links = []
+    img_src_links = []
+    while len(post_links) < n:
+        links = [a.get_attribute('href') for a in browser.find_elements_by_tag_name('a')]
+        images = [img.get_attribute('src') for img in browser.find_elements_by_css_selector('article img')]
+        for i, link in enumerate(links):
+            if post in link and link not in post_links:
+                post_links.append(link)
+            
+            img_src_links.append(images[i])
+            
+        
+        scroll_down = "window.scrollTo(0, document.body.scrollHeight);"
+        browser.execute_script(scroll_down)
+        time.sleep(10)
+    else:
+        return post_links[:n], img_src_links[:n]
+    
+
+def get_hashtags(url):
+    browser = Chrome()
+    browser.get(url)
+    comments_html = browser.find_elements_by_css_selector('span')
+    all_hashtags = []
+    
+    for comment in comments_html:
+        hashtags = re.findall('#[A-Za-z]+', comment.text)
+        if len(hashtags) > 0:
+            all_hashtags.extend(hashtags)
+    return list(set(all_hashtags))
+        
+    
+def get_images_urls(url):
+    browser = Chrome()
+    browser.get(url)    
+    images = browser.find_elements_by_CSS_selector('article img')
+    img_links = []
+    for image in images:
+        img_links.append(image.get_attribute('src'))
+    return img_links
+ 
